8e724ee41f49aa7881cdbe6fb60a706773f87bae
Tommy <tommy.choa@foodpanda.com>
2023-03-28 23:25:26 +0800

    merge

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -4,22 +4,45 @@ import ProductCard from "./ProductCard";
 const ProductList = () => {
   return (
     <div className={styles.container}>
-<<<<<<< HEAD
-      <h1 className={styles.title} id="availableProducts">
-        Available Products
-      </h1>
-      <p className={styles.desc}>Brief description of products.</p>
-=======
       <h1 className={styles.title}>Available Products</h1>
       {/* <p className={styles.desc}>Brief description of products.</p> */}
->>>>>>> 12f58e7e66968cab9709753e834227d41bc552e9
       <div className={styles.wrapper}>
-        <ProductCard path='/img/monstera.png' title='Monstera Deliciosa' price='40.00' description='Also called the Swiss cheese plant, easy to care form weekly waterings, indoor friendly.'/>
-        <ProductCard path='/img/peacelily.png' title='Peace Lily' price='25.00' description='Peace Lily is easy to care for, weekly waterings, indoor friendly.'/>
-        <ProductCard path='/img/plant.png' title='Pothos' price='10.00' description='Pothos plant is easy to care for, can be fully submerged, indoor friendly.'/>
-        <ProductCard path='/img/snakeplant.png' title='Snake Plant' price='15.00' description='Snake plant is easy to care for, bi-weekly waterings, indoor friendly.'/>
-        <ProductCard path='/img/spiderplant.png' title='Spider Plant' price='10.00' description='Spider plant is easy to carefor, weekly waterings, indoor friendly.'/>
-        <ProductCard path='/img/zzplant.png' title='ZZ Plant' price='30.00' description='ZZ plant is easy to care for, slow growing, bi-weekly waterings, indoor and outdoor friendly.'/>
+        <ProductCard
+          path="/img/monstera.png"
+          title="Monstera Deliciosa"
+          price="40.00"
+          description="Also called the Swiss cheese plant, easy to care form weekly waterings, indoor friendly."
+        />
+        <ProductCard
+          path="/img/peacelily.png"
+          title="Peace Lily"
+          price="25.00"
+          description="Peace Lily is easy to care for, weekly waterings, indoor friendly."
+        />
+        <ProductCard
+          path="/img/plant.png"
+          title="Pothos"
+          price="10.00"
+          description="Pothos plant is easy to care for, can be fully submerged, indoor friendly."
+        />
+        <ProductCard
+          path="/img/snakeplant.png"
+          title="Snake Plant"
+          price="15.00"
+          description="Snake plant is easy to care for, bi-weekly waterings, indoor friendly."
+        />
+        <ProductCard
+          path="/img/spiderplant.png"
+          title="Spider Plant"
+          price="10.00"
+          description="Spider plant is easy to carefor, weekly waterings, indoor friendly."
+        />
+        <ProductCard
+          path="/img/zzplant.png"
+          title="ZZ Plant"
+          price="30.00"
+          description="ZZ plant is easy to care for, slow growing, bi-weekly waterings, indoor and outdoor friendly."
+        />
       </div>
     </div>
   );
